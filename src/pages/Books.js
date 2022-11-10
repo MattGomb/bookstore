5ea81e241437d3f63cc0d3160272da3ef1605285
Matyas Gombos <gombosmm@gmail.com>
2022-11-10 11:05:32 -0500

    Add structure of books main page

---
+++ src/pages/Books.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Books() {
+  return (
+    <section className="books-container">
+      <List />
+      <Form />
+    </section>
+  );
+}
+
+export default Books;
