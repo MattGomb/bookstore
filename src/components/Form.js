95f2c1d03d9805cfb4d58b8b45309a5ac439f2f4
Matyas Gombos <gombosmm@gmail.com>
2022-11-10 13:31:15 -0500

    Adjust format and structure

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,12 +2,16 @@ import React from "react";
 
 function Form() {
   return (
-    <form className="form-container">
-      <h2 className="form-title">ADD NEW BOOK</h2>
-      <input type="text" placeholder="Book title" />
-      <input type="text" placeholder="Author" />
-      <button type="submit">ADD BOOK</button>
-    </form>
+    <section className="form-container">
+      <h2 className="form-title">
+        ADD NEW BOOK
+      </h2>
+      <form className="form">
+        <input type="text" placeholder="Book title" />
+        <input type="text" placeholder="Author" />
+        <button type="submit" className="add-button">ADD BOOK</button>
+      </form>
+  </section>
   );
 }
 
