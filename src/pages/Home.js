c87f7f20e95bf9d1d0c4bc918b50fbf1aabde3c5
Matyas Gombos <gombosmm@gmail.com>
2022-11-10 15:40:27 -0500

    Add 'home' page

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Welcome to the BOOKSTORE!</h1>
+      <p className="home-p">Browse to your heart&apos;s content!</p>
+    </div>
+  );
+}
+
+export default Home;
