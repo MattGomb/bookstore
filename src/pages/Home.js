48d8819cd14c5b169e0a61c5bedff527ba947f0e
Matyas Gombos <gombosmm@gmail.com>
2022-11-10 17:05:59 -0500

    Adjust styling of homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 function Home() {
   return (
     <div className="home-container">
-      <h1 className="home-title">Welcome to the BOOKSTORE!</h1>
-      <p className="home-p">Browse to your heart&apos;s content!</p>
+      <div className="home-content">
+        <h1 className="home-title">Welcome to the BOOKSTORE!</h1>
+        <p className="home-p">Browse to your heart&apos;s content!</p>
+      </div>
     </div>
   );
 }
