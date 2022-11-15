8dd56bd4d618b941b7ec0875172bba6aa4f4e7d9
Matyas Gombos <gombosmm@gmail.com>
2022-11-15 16:19:13 -0500

    Add addbook function to form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,41 @@
 import { React, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { v4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
 const Form = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const id = v4().slice(0, 10);
+  const progress = Math.floor(Math.random() * 100);
+  const currentChapter = Math.floor(Math.random() * 10);
+  const dispatch = useDispatch();
+
+  const onChangeAuthorHandler = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const onChangeTitleHandler = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const onClickAddBook = () => {
+    dispatch(addBook({
+      id, title, author, progress, currentChapter,
+    }));
+    setTitle('');
+    setAuthor('');
+  };
+
   return (
     <section className="form-container">
       <h2 className="form-title">
         ADD NEW BOOK
       </h2>
       <form className="form">
-        <input type="text" placeholder="Book title" />
-        <input type="text" placeholder="Author" />
-        <button type="submit" className="add-button">ADD BOOK</button>
+        <input type="text" placeholder="Book title" onChange={onChangeTitleHandler} />
+        <input type="text" placeholder="Author" onChange={onChangeAuthorHandler} />
+        <button type="button" className="add-button" onClick={onClickAddBook}>ADD BOOK</button>
       </form>
     </section>
   );
