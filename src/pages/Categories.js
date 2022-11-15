bb2858c9a3a585d9db155722039c59a446a3581f
Matyas Gombos <gombosmm@gmail.com>
2022-11-15 16:34:07 -0500

    Link categories redux component to page

--- src/pages/Categories.js
+++ src/pages/Categories.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../redux/categories/categories';
 
 function Categories() {
+  const categories = useSelector((state) => state.categories);
+  const dispatch = useDispatch();
+  const onClickStatus = (e) => {
+    dispatch(checkStatus());
+    e.currentTarget.classList.toggle('add-button');
+  };
+
   return (
     <section className="categories-container">
-      <button type="button">Check Status</button>
+      <button type="button" onClick={onClickStatus}>Check Status</button>
+      <p>{categories}</p>
     </section>
   );
 }
