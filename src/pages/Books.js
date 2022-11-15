efe131132e717227ed48103f3df4d401bdce3ee6
Matyas Gombos <gombosmm@gmail.com>
2022-11-15 16:20:32 -0500

    Change to arrow function

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -2,13 +2,11 @@ import React from 'react';
 import List from '../components/List';
 import Form from '../components/Form';
 
-function Books() {
-  return (
-    <section className="books-container">
-      <List />
-      <Form />
-    </section>
-  );
-}
+const Books = () => (
+  <section className="books-container">
+    <List />
+    <Form />
+  </section>
+);
 
 export default Books;
