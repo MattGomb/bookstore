bce87c6567c42ee684393a54991b72148aeeac6a
Matyas Gombos <gombosmm@gmail.com>
2022-11-15 16:20:08 -0500

    Adjust premade part of list to map through prepared books

--- src/components/List.js
+++ src/components/List.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 
 function List() {
+  const books = useSelector((state) => state.books);
   return (
     <section className="list-container">
       <div className="list">
-        <Book />
-        <Book />
-        <Book />
+        {books.map((book) => (
+          <Book
+            key={book.id}
+            id={`${book.id}`}
+            title={book.title}
+            author={book.author}
+            progress={book.progress}
+            currentChapter={book.currentChapter}
+          />
+        ))}
       </div>
       <span className="divider" />
     </section>
