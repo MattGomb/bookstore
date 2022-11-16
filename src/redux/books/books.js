5978ade01117ff78ebc0abe63f73ae427cc7f40a
Matyas Gombos <gombosmm@gmail.com>
2022-11-16 18:23:50 -0500

    Add API methods

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,33 +1,16 @@
+// specific external API
+
+const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/Ob3CSqKLG46FjtXBtUgx/books';
+
 // actions
 
 const ADD = 'bookstore/src/redux/books/books/ADD';
 const REMOVE = 'bookstore/src/redux/books/books/REMOVE';
+const FETCH = 'FETCH';
 
 // reducer
 
-const defaultState = [
-  {
-    id: 1,
-    title: 'Harry Potter and the Philosopher\'s Stone',
-    author: 'J.K. Rowling',
-    progress: '64',
-    currentChapter: ' 17',
-  },
-  {
-    id: 2,
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
-    progress: '8',
-    currentChapter: ' 3',
-  },
-  {
-    id: 3,
-    title: 'The Lord of the Rings',
-    author: 'J.R.R. Tolkien',
-    progress: '99',
-    currentChapter: ' 24',
-  },
-];
+const defaultState = [];
 
 export default function booksReducer(state = defaultState, action) {
   switch (action.type) {
@@ -44,12 +27,34 @@ export default function booksReducer(state = defaultState, action) {
       ];
     case REMOVE:
       return state.filter((book) => book.id !== action.id);
+    case FETCH:
+      return action.list;
     default:
       return state;
   }
 }
 
-// action creators for add and remove books
+// action creators for fetch, add and remove books
+
+export const fetchBook = (list) => ({ type: FETCH, list });
+
+export const fetchBookAsync = () => async (dispatch) => {
+  await fetch(URL)
+    .then((response) => response.json())
+    .then((books) => {
+      const list = [];
+      Object.keys(books).forEach((key) => {
+        list.push({
+          id: key,
+          title: books[key][0].title,
+          author: books[key][0].author,
+          progress: books[key][0].progress,
+          currentChapter: books[key][0].currentChapter,
+        });
+      });
+      dispatch(fetchBook(list));
+    });
+};
 
 export const addBook = (payload) => ({
   type: ADD,
@@ -60,7 +65,36 @@ export const addBook = (payload) => ({
   currentChapter: payload.currentChapter,
 });
 
+export const bookFetch = ({
+  id, title, author, progress, currentChapter,
+}) => async (dispatch) => {
+  const newBook = {
+    id,
+    title,
+    author,
+    progress,
+    currentChapter,
+  };
+  await fetch(URL, {
+    method: 'POST',
+    body: JSON.stringify(newBook),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  dispatch(addBook(newBook));
+};
+
 export const removeBook = (payload) => ({
   type: REMOVE,
   id: payload.id,
 });
+
+export const bookRemove = (id) => async (dispatch) => {
+  await fetch(`${URL}/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then(() => dispatch(removeBook({ id })));
+};
