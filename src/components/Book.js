dc918a3db7488d97b0de1b24f5a7d12555a0b967
Matyas Gombos <gombosmm@gmail.com>
2022-11-17 13:53:49 -0500

    Add random chapter and progress functionality

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,31 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import RemoveBook from './RemoveBook';
 
 const Book = ({
   id, category, title, author,
-}) => (
-  <div className="book">
-    <div className="book-info">
-      <p>{category}</p>
-      <h1>{title}</h1>
-      <p>{author}</p>
-      <div className="actions">
-        <button type="button" className="book-button">Comments</button>
-        <RemoveBook id={id} />
-        <button type="button" className="book-button">Edit</button>
+}) => {
+  const percentage = Math.floor(Math.random() * 101);
+  const chapter = Math.floor(Math.random() * 20 + 1);
+  return (
+    <div className="book">
+      <div className="book-info">
+        <p>{category}</p>
+        <h1>{title}</h1>
+        <p>{author}</p>
+        <div className="actions">
+          <button type="button" className="book-button">Comments</button>
+          <RemoveBook id={id} />
+          <button type="button" className="book-button">Edit</button>
+        </div>
+      </div>
+      <div className="book-progress">
+        <CircularProgressbar id="progress-bar" value={percentage} text={`${percentage}%`} />
+      </div>
+      <div className="book-current">
+        <h3>CURRENT CHAPTER</h3>
+        <p>
+          Chapter
+          {chapter}
+        </p>
+        <button type="button" className="progress-button">UPDATE PROGRESS</button>
       </div>
     </div>
-    <div className="book-progress">
-      <h3>47%</h3>
-    </div>
-    <div className="book-current">
-      <h3>CURRENT CHAPTER</h3>
-      <p>Chapter 7</p>
-      <button type="button" className="progress-button">UPDATE PROGRESS</button>
-    </div>
-  </div>
-);
+  );
+};
 
 Book.propTypes = {
   id: PropTypes.string.isRequired,
