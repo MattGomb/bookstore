e7a8d654727b69200fcbc5840c8e0360dbd37e8d
Matyas Gombos <gombosmm@gmail.com>
2022-11-16 20:57:22 -0500

    Update form and select options

--- src/components/Form.js
+++ src/components/Form.js
@@ -26,12 +26,13 @@ const Form = () => {
 
   const onClickAddBook = (e) => {
     e.preventDefault();
-    if (document.getElementById('title-input').value !== '' && document.getElementById('author-input').value !== '') {
+    if (document.getElementById('title-input').value !== '' && document.getElementById('author-input').value !== '' && document.getElementById('category-input').value !== 'Choose') {
       dispatch(bookFetch({
         id, title, author, category, progress, currentChapter,
       }));
       document.getElementById('title-input').value = '';
       document.getElementById('author-input').value = '';
+      document.getElementById('category-input').value = 'Choose';
     }
   };
 
@@ -44,13 +45,20 @@ const Form = () => {
         <input name="title-input" id="title-input" type="text" placeholder="Book title" onChange={onChangeTitleHandler} />
         <input name="author-input" id="author-input" type="text" placeholder="Author" onChange={onChangeAuthorHandler} />
         <select name="category-input" id="category-input" onChange={onChangeCategoryHandler}>
+          <option value="Choose">choose</option>
           <option value="Action">Action</option>
           <option value="Biography">Biography</option>
+          <option value="Fantasy">Fantasy</option>
           <option value="History">History</option>
           <option value="Horror">Horror</option>
           <option value="Kids">Kids</option>
           <option value="Learning">Learning</option>
+          <option value="Science">Science</option>
           <option value="Sci-Fi">Sci-Fi</option>
+          <option value="Self-Help">Self-Help</option>
+          <option value="Thriller">Thriller</option>
+          <option value="Western">Western</option>
+          <option value="Other">Other</option>
         </select>
         <button type="submit" className="add-button" onClick={onClickAddBook}>ADD BOOK</button>
       </form>
