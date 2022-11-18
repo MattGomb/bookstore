67617ad8bc5db08c865c00adcce0297773b8178f
Matyas Gombos <gombosmm@gmail.com>
2022-11-18 09:27:05 -0500

    Match remove book function

--- src/components/RemoveBook.js
+++ src/components/RemoveBook.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { bookRemove } from '../redux/books/books';
+import { removeBook } from '../redux/books/books';
 
 const RemoveBook = (id) => {
   const dispatch = useDispatch();
   const onClickRemoveBook = () => {
-    dispatch(bookRemove(id));
+    console.log(dispatch(removeBook(id)));
+    dispatch(removeBook(id));
   };
   return (
     <button type="button" className="book-button" onClick={onClickRemoveBook}>
